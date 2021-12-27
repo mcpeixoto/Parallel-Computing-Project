77a20214bc236430cafd0c8461345f53eb5a12f1
Miguel Peixoto a92838@alunos.uminho.pt <a92838@search7edu.local>
2021-12-27 21:11:30 +0000

    Habemus results

--- para_entregar/12_bucket_sort_paralelism_schedule_guided/time5x.sh
+++ para_entregar/12_bucket_sort_paralelism_schedule_guided/time5x.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-MY=/home/a92838/Trabalho/para_entregar/1_bucket_sort_paralelism_omp_parallel_for_sort_buckets
+MY=/home/a92838/Trabalho/para_entregar/12_bucket_sort_paralelism_schedule_guided
 
 time for i in {1..5}; do $MY/bucket.out; done
