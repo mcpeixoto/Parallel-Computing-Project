655ba548da7b0737ea1a453c54145839e1953d3c
Miguel Peixoto a92838@alunos.uminho.pt <a92838@search7edu.local>
2021-12-29 14:25:20 +0000

    corrected time path and re-ran results.csv on 4_quick_sort_3

--- para_entregar/parallel/4_quick_sort_3/time5x.sh
+++ para_entregar/parallel/4_quick_sort_3/time5x.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-MY=/home/a92841/Trabalho-Programa--o-Paralela/para_entregar/parallel/4_quick_sort_2
+MY=/home/a92838/Trabalho/para_entregar/parallel/4_quick_sort_3
 
 time for i in {1..5}; do $MY/bucket.out; done
